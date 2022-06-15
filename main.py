8595425549c137f0200d9ef3184efcf02db0c8dd
President Foxman <darklink2458@users.noreply.github.com>
2022-06-14 17:49:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,42 @@ import lightbulb
 #my_secret = os.environ['TOKEN']
 
 #bot = lightbulb.BotApp(token=my_secret)
-bot = lightbulb.BotApp(token="OTQ0ODQwMDk3OTQ4MzkzNTAy.YhHc6Q.0LZoNE3Ds0f26f_r_CUahydW0mo")
+bot = lightbulb.BotApp(
+    token="OTQ0ODQwMDk3OTQ4MzkzNTAy.YhHc6Q.0LZoNE3Ds0f26f_r_CUahydW0mo",
+    default_enabled_guilds=(788564875658919947)
+)
 
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def ping(event) -> None:
-    # If a non-bot user sends a message "hk.ping", respond with "Pong!"
-    # We check there is actually content first, if no message content exists,
-    # we would get `None' here.
-    if event.is_bot or not event.content:
-        return
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
 
-    if event.content.startswith("hk.ping"):
-        await event.message.respond("Pong!")
+@bot.command
+@lightbulb.option('num2', 'The second number', type=int)
+@lightbulb.option('num1', 'The first number', type=int)
+@lightbulb.command('add', 'Add two numbers together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+    await ctx.respond(ctx.options.num1 + ctx.options.num2)
 
-@bot.listen(hikari.StartedEvent)
-async def bot_started(event):
-    print('The Jester Has Arrived!')
+@bot.command
+@lightbulb.add_checks(lightbulb.owner_only)
+@lightbulb.command("foo", "test command")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def foo(ctx):
+    await ctx.respond("You are the owner of this bot.")
+
+@bot.command
+@lightbulb.command('my_group', 'This is a group')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def my_group(ctx):
+    pass
+
+@my_group.child
+@lightbulb.command('subcommand', 'This is a subcommand')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def subcommand(ctx):
+    await ctx.respond('I am a subcommand!')
 
 bot.run()
